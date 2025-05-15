0c8fffab14c0ca5402120928e4d7d492baf15821
Robin-J-V <87322649+Robin-J-V@users.noreply.github.com>
2025-05-15 14:42:07 -0400

    connected to mongodb
    connected to mongodb

---
+++ db.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri);
+
+let db;
+
+async function connectToDB() {
+  try {
+    await client.connect();
+    db = client.db('cs355db');
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting MongoDB:", err);
+  }
+}
+
+function getDB() {
+  if (!db) {
+    throw new Error("DB not initialized. Call connectToDB first.");
+  }
+  return db;
+}
+
+module.exports = { connectToDB, getDB };
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const { connectToDB } = require('./db');
+
+connectToDB();
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
